347f14067eecdafe3f295dd8d90df50b42b1a167
PG Kim <vakon1@daum.net>
2025-02-13 10:37:54 +0900

    새로운 프로젝트 생성

---
+++ src/hello/Hellojava.java
@@ -0,0 +1,24 @@
+package hello;
+
+/* 
+ * 주석 여러줄
+ * 작성자: 이~슬
+ * 작성일: 02/12
+ * */
+public class Hellojava {
+
+	// 주석이란? 프로그램의 설명
+	// 메인함수: 프로그램의 시작점
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// 문장을 출력하는 코드(괄호안에 있는 문자를 출력)
+		// 문장을 완성할때는 세미콜론(;)으로 마무리
+		// 문법 에러가 발생되면 빨간색으로 표시됨
+		System.out.println("leeseul, hi~");
+		
+		// ctrl+s 저장	
+		
+	}
+
+}
